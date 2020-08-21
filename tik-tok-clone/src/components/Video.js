cf096cfc5b3a4cb607f0cb5842d3a9d834cbfe5f
Carlos Garcia Castellanos <chars24@gmail.com>
2020-08-21 14:22:02 -0500

    1.2 Footer

--- tik-tok-clone/src/components/Video.js
+++ tik-tok-clone/src/components/Video.js
@@ -1,4 +1,7 @@
 import React, { useRef, useState } from 'react'
+
+import VideoFooter from './VideoFooter'
+
 import './Video.css'
 
 const Video = () => {
@@ -23,7 +26,7 @@ const Video = () => {
         onClick={onVideoPress}
         ref={videoRef}
         src="https://v16m.tiktokcdn.com/cbb043ec55f099892f3da70bd69eb5dd/5f4294e0/video/tos/useast2a/tos-useast2a-pve-0068/365c008e52244146977a6d63ce2988e1/?a=1233&br=2828&bt=1414&cr=0&cs=0&dr=0&ds=3&er=&l=202008211610080101890741485827E911&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajhpM3JvbnBwdTMzZjczM0ApOTVpNjU2N2U5NzwzaGQ0OmcwZjNgcnNrYzVfLS00MTZzc181M2EuMC02YTAxMF5jYWE6Yw%3D%3D&vl=&vr="></video>
-      {/*<VideoFooter> */}    
+      <VideoFooter channel="chars34" description="send it by props" song="la macarena - del rio"/>  
       {/*<VideoSidebar> */}    
     </div>
   )
