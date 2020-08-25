f9ce0a6d90383fe431a9ba344749160752a314af
Carlos Garcia Castellanos <chars24@gmail.com>
2020-08-25 12:20:47 -0500

    1.4 Firebase and deploying

--- tik-tok-clone/src/App.js
+++ tik-tok-clone/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import db from './firebase'
+
 import './App.css';
 
 // Components
 import Video from './components/Video'
 
 function App() {
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    db.collection("videos").onSnapshot((snapshot) =>
+      setVideos(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, []);
+
   return (
     // BEM
     <div className="app">
         <div className="app__videos">
-          <Video
-            url="https://v16m.tiktokcdn.com/a717d72978f8a49475d79659381553f4/5f42cc32/video/tos/useast2a/tos-useast2a-pve-0068/8eef09854d3a4aa383741e44b0c49838/?a=1233&br=2812&bt=1406&cr=0&cs=0&dr=0&ds=3&er=&l=202008212006100101890651374742F4DC&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M287aXdnMzx2djMzaTczM0ApOmg5aTo1NGVlNzg5aWRkNWdrai8yaGZgMGxfLS1hMTZzcy8zMmJgYjFjYjUtLzE2MGA6Yw%3D%3D&vl=&vr="
-            channel="hegaan"
-            description="WOW this work"
-            song="La bomba - Ricky Martin"
-            likes={123}
-            messages={400}
-            shares={200}
-          />
-          <Video />
-          <Video />
+          {videos.map(({url, channel, description, song, likes, messages, shares}) => (
+            <Video 
+              url = {url}
+              channel = {channel}
+              song = {song}
+              likes = {likes}
+              messages = {messages}
+              description = {description}
+              shares = {shares}
+            />)
+          )}
         </div>
     </div>
   );
--- tik-tok-clone/src/firebase.js
+++ tik-tok-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: "AIzaSyDWN0VAip529d68h6fcpenp0k-TVMTirEA",
   authDomain: "tik-tok-clone-ac564.firebaseapp.com",
@@ -8,4 +10,8 @@ const firebaseConfig = {
   appId: "1:74202210865:web:9401ac2efa9563aace2b10"
 };
 
-export default firebaseConfig
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore()
+
+export default db
